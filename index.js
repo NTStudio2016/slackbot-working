6d492c5c0c6991c7b6f41709825a5989d3b8bf59
NTStudio2016 <n007.tamas@gmail.com>
2020-02-12 08:04:25 +0100

    1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const SlackBot = require('slackbots');
 const axios = require('axios');
 
 const bot = new SlackBot({
-  token: 'xoxb-946011727382-931097051394-2MZnRt1NCPGbNni9LOH2hVUf',
+  token: 'xoxb-946011727382-931097051394-bxiy6HaBvIQnuzgZhPmxTip4',
   name: 'bot'
 });
 
@@ -110,6 +110,62 @@ function handleMessage(message, username, usersender, channelidforbot){
             bot.postMessageToChannel('general', `Szia ${name} a botnak a következő parancsai vannak *@Bot segítség*`, params);
         });
     }
+    else if(message.includes(' emlékeztető')){
+        let darabok = message.split(" ");
+        //respond to add command
+        if(darabok[2] === 'hozzáad'){
+            //chenck if there  are items to add
+            if(darabok.length > 3){
+                let kuldeni = [];
+                for (let i = 3; i < darabok.length; i++) {
+                    kuldeni.push(darabok[i]);
+                }
+                AddToReminder(kuldeni);
+            }
+            else{
+                const params = {
+                    icon_emoji: ':face_with_rolling_eyes:'
+                };
+                bot.postMessageToChannel('general', `Nem adtál meg elemet a hozzáadáshoz!`, params);
+            }
+        }
+        //respond to chenck command
+        else if(darabok[2] === 'megnéz'){
+            ViewReminder();
+        }
+        //respond to reove command
+        else if(darabok[2] === 'töröl'){
+            //check if there are items to delete
+            if(darabok.length > 3){
+                //check if shopping list   is empty ot not
+                if(bevasarlolista.length !== 0){
+                    let kuldeni = [];
+                    for (let i = 3; i < darabok.length; i++) {
+                        kuldeni.push(darabok[i]);
+                    }
+                    RemoveFromReminder(kuldeni);
+                }
+                else{
+                    const params = {
+                        icon_emoji: ':face_with_rolling_eyes:'
+                    };
+                    bot.postMessageToChannel('general', `Az emlékeztetőnek nincsenek elemei!`, params);
+                }
+            }
+            else{
+                const params = {
+                    icon_emoji: ':face_with_rolling_eyes:'
+                };
+                bot.postMessageToChannel('general', `Nem adtál meg elemet a törléshez!`, params);
+            }
+        }
+        else{
+            const params = {
+                icon_emoji: ':face_with_rolling_eyes:'
+            };
+            bot.postMessageToChannel('general', `A emlékeztetőnek nincs ilyen parancsa: ${darabok[2]}`, params);
+        }
+    }
     else if(username !== 'bot'){
         bot.getUserId('bot').then(res => {
             let user = res;
@@ -202,4 +258,56 @@ function Help(){
             });
         });
     });
+}
+
+let reminder = [];
+
+function AddToReminder(hozzaadni = []){
+    for(let i = 0; i < hozzaadni.length; i++){
+        reminder.push(hozzaadni[i]);
+    }
+    const params = {
+        icon_emoji: ':package:'
+    };
+    bot.postMessageToChannel('general', `Az emlékeztetőben lévő jelenlegi elemek: *@Bot emlékeztető megnéz*`, params);
+}
+
+function RemoveFromReminder(torolnivalo = []){
+    let tempList = []
+    let torolt = 0;
+    for(let i = 0; i < reminder.length; i++){
+        for(let j = 0; j < torolnivalo.length; j++){
+            if(reminder[i] !== torolnivalo[j]){
+                tempList.push(reminder[i]);
+            }
+            else{
+                torolt++;
+            }
+        }
+    }
+    reminder = [];
+    reminder = tempList;
+    const params = {
+        icon_emoji: ':negative_squared_cross_mark:'
+    };
+    bot.postMessageToChannel('general', `Az emlékeztetőről töröltél: ${torolt} elemet`, params);
+}
+function ViewReminder(){
+    if(reminder.length !== 0){
+        const params = {
+            icon_emoji: ':shopping_trolley:'
+        };
+    
+        let kimeno = 'Teendők az emlékeztetőben: ';
+        for (let i = 0; i < reminder.length; i++) {
+            kimeno += reminder[i] + ', ';
+        }
+        bot.postMessageToChannel('general', kimeno, params);
+    }
+    else{
+        const params = {
+            icon_emoji: ':thumbsup:'
+        };
+        bot.postMessageToChannel('general', `Nincsenek teendők az emlékeztetőben`, params);
+    }
 }
